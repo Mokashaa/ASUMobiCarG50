028b6dc656fbf7fa1a46ffdfdf61a85e9a0faa58
Mohamad Okasha <36459281+Mokashaa@users.noreply.github.com>
2018-02-19 00:15:00 +0200

    Add files via upload

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,6 +1,21 @@
+char order;
+int A1 = ; //A1 is the 1st right motors pin
+int B1 = ; //B1 is the 2nd right motors pin
+int A2 = ; //A2 is the 1st left motors pin
+int B2 = ; //B2 is the 2nd left motors pin
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
   // besmallah
+   pinMode(ENA, OUTPUT); //Enable pins
+  pinMode(ENB, OUTPUT); //Enable pins
+  pinMode(A1, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B2, OUTPUT);
   
 }
 
