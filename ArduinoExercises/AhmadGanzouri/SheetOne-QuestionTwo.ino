fdcf6d819e9e7e86107ddd9692be8ae67d7ad2b7
Ahmad Ganzouri <ganzouri97@gmail.com>
2018-02-19 23:17:38 +0200

    sheetone-questiontwo code
    making the 4 leds go through a loop from right to left then from left to right and so on.

---
+++ ArduinoExercises/AhmadGanzouri/SheetOne-QuestionTwo.ino
@@ -0,0 +1,46 @@
+int LED1 = 4;
+int LED2 = 5;
+int LED3 = 6;
+int LED4 = 7;
+int ONTime = 1000;  // delay time 1 second.
+
+void setup() {
+pinMode(LED1,OUTPUT);
+pinMode(LED2,OUTPUT);  //All pins are output 
+pinMode(LED3,OUTPUT);
+pinMode(LED3,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,LOW);         // LED1 ON
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  delay(ONTime);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,LOW);         // LED2 ON
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  delay(ONTime);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);        //LED3 ON
+  digitalWrite(LED4,LOW);
+  delay(ONTime);
+  digitalWrite(LED4,HIGH);
+  digitalWrite(LED2,LOW);        // LED4 ON
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED1,LOW);
+  delay(ONTime);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED1,LOW);        //LED3 ON
+  digitalWrite(LED4,LOW);
+  delay(ONTime);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,LOW);       // LED2 ON
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  delay(ONTime);
+}
