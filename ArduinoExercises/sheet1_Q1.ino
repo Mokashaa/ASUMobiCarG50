7dd024b6d274b40c7df1825b4fe5dc639dffbe90
DohaMustafa <36457313+DohaMustafa@users.noreply.github.com>
2018-02-19 20:17:43 +0200

    Add files via upload

---
+++ ArduinoExercises/sheet1_Q1.ino
@@ -0,0 +1,15 @@
+int ledPin =9;
+int ledOn=500;
+int ledOff=1500;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(ledPin,HIGH);
+  delay(ledOn);
+  digitalWrite(ledPin,LOW);
+  delay(ledOff);
+}
