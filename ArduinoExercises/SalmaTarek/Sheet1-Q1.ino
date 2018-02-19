e97da5b352fd6ec127bc7f829bec008377a9548a
Salma Tarek <1500667@eng.asu.edu.eg>
2018-02-19 22:58:50 +0200

    Update Sheet1-Q1.ino

--- ArduinoExercises/SalmaTarek/Sheet1-Q1.ino
+++ ArduinoExercises/SalmaTarek/Sheet1-Q1.ino
@@ -1,14 +1,12 @@
 #define LED 7
-void setup() {
-  
-  pinMode(LED, OUTPUT)
 
+void setup() {  
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-
 digitalWrite(LED, HIGH);
-delay(2000);
+delay(2000); //ON Time
 digitalWrite(LED, LOW);
-delay(6000);
+delay(6000); //OFF Time
 }
