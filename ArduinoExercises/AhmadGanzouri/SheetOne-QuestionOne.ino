ed322cbeaa87ed1682a6eec67cf908bd47732262
Ahmad Ganzouri <ganzouri97@gmail.com>
2018-02-20 01:08:32 +0200

    Updating ...

--- ArduinoExercises/AhmadGanzouri/SheetOne-QuestionOne.ino
+++ ArduinoExercises/AhmadGanzouri/SheetOne-QuestionOne.ino
@@ -1,9 +1,9 @@
-  int LED = 13; // using the internal LED of the arduino at pin 13 
+  int LED = 13; // using the internal  LED of the arduino at pin 13 or external led..
   int ONTime = 2000; // 2 seconds delay
   int OFFTime = 6000; // 6 seconds delay 
   
 void setup() {
-Serial.begin(9600);
+
 
 pinMode(LED,OUTPUT); // type of the pin is output
   
@@ -13,8 +13,8 @@ void loop() {
 
   digitalWrite(LED,HIGH); 
   delay(ONTime);
-  Serial.println("ON"); // to make sure of the time 
+
   digitalWrite(LED,LOW);
   delay(OFFTime);
-  Serial.println("OFF"); // same as the previous 
+  
 }
