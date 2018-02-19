8a2af59c531e04f49ae5b2806d00078292d59ec4
Salma Tarek <1500667@eng.asu.edu.eg>
2018-02-19 23:13:11 +0200

    Add files via upload

---
+++ ArduinoExercises/SalmaTarek/Sheet1-Q2.ino
@@ -0,0 +1,22 @@
+int LED [] = {4,5,6,7};
+int x = 0;
+
+void setup() {
+  for (x = 0; x < 4; x++)
+  {
+    pinMode(LED[x],OUTPUT);
+  }
+}
+
+void loop() {
+  for(x = 0; x < 4; x++)
+  {
+    digitalWrite(LED[x],HIGH);
+    delay(1000);
+  }
+  for(x = 3; x >= 0; x--)
+  {
+    digitalWrite(LED[x],HIGH);
+    delay(1000);
+  }
+}
