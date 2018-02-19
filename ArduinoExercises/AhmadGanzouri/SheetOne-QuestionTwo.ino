047c54e273ca85f4c704ed1400695b6e5bf93995
Ahmad Ganzouri <ganzouri97@gmail.com>
2018-02-20 00:55:15 +0200

    Minimizing the code

--- ArduinoExercises/AhmadGanzouri/SheetOne-QuestionTwo.ino
+++ ArduinoExercises/AhmadGanzouri/SheetOne-QuestionTwo.ino
@@ -2,45 +2,38 @@ int LED1 = 4;
 int LED2 = 5;
 int LED3 = 6;
 int LED4 = 7;
-int ONTime = 1000;  // delay time 1 second.
 
 void setup() {
 pinMode(LED1,OUTPUT);
 pinMode(LED2,OUTPUT);  //All pins are output 
 pinMode(LED3,OUTPUT);
-pinMode(LED3,OUTPUT);
+pinMode(LED4,OUTPUT);
 
 }
 
 void loop() {
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,LOW);         // LED1 ON
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED4,LOW);
-  delay(ONTime);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED1,LOW);         // LED2 ON
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED4,LOW);
-  delay(ONTime);
-  digitalWrite(LED3,HIGH);
+  digitalWrite(LED1,HIGH);    // LED1 ON
+  delay(1000);
+  
   digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);        //LED3 ON
-  digitalWrite(LED4,LOW);
-  delay(ONTime);
-  digitalWrite(LED4,HIGH);
-  digitalWrite(LED2,LOW);        // LED4 ON
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED1,LOW);
-  delay(ONTime);
-  digitalWrite(LED3,HIGH);
+  digitalWrite(LED2,HIGH);        // LED2 ON
+  delay(1000);
+
   digitalWrite(LED2,LOW);
-  digitalWrite(LED1,LOW);        //LED3 ON
-  digitalWrite(LED4,LOW);
-  delay(ONTime);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED1,LOW);       // LED2 ON
+  digitalWrite(LED3,HIGH);    //LED3 ON
+  delay(1000);
+
   digitalWrite(LED3,LOW);
+  digitalWrite(LED4,HIGH);   //LED4 ON
+  delay(1000);
+
   digitalWrite(LED4,LOW);
-  delay(ONTime);
+  digitalWrite(LED3,HIGH);  // LED3 ON
+  delay(1000);
+
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED2,HIGH);       // LED2 ON
+  delay(1000);
+  
+  digitalWrite(LED2,LOW);
 }
