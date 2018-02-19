9e3aa4ba979f243c262147cf6ba80e49736dfd10
Ahmad Ganzouri <ganzouri97@gmail.com>
2018-02-19 22:53:56 +0200

    sheetone-questionone code
    Making the internal led of the arduino high for 2 second and low for 6 second under condition -duty cycle = 0.25- 
    Thigh = 2seconds , Ttotal = 8 seconds.

---
+++ ArduinoExercises/AhmadGanzouri/SheetOne-QuestionOne.ino
@@ -0,0 +1,20 @@
+  int LED = 13; // using the internal LED of the arduino at pin 13 
+  int ONTime = 2000; // 2 seconds delay
+  int OFFTime = 6000; // 6 seconds delay 
+  
+void setup() {
+Serial.begin(9600);
+
+pinMode(LED,OUTPUT); // type of the pin is output
+  
+}
+
+void loop() {
+
+  digitalWrite(LED,HIGH); 
+  delay(ONTime);
+  Serial.println("ON"); // to make sure of the time 
+  digitalWrite(LED,LOW);
+  delay(OFFTime);
+  Serial.println("OFF"); // same as the previous 
+}
