2a93574aafdec8457439a028c5ab41086c601359
EmaMOUKH <36458600+EmaMOUKH@users.noreply.github.com>
2018-02-20 21:06:28 +0200

    Add files via upload

---
+++ ArduinoExercises/Eman Moukhtar/sketch_feb20a.ino
@@ -0,0 +1,26 @@
+int redLED=9;
+int yellowLED=10;
+int greenLED=11;
+
+void setup() {
+  pinMode(redLED,OUTPUT);
+  pinMode(yellowLED,OUTPUT);
+  pinMode(greenLED,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(redLED,HIGH);
+  delay(3000);
+  digitalWrite(yellowLED,HIGH);
+  delay(1000);
+  digitalWrite(redLED,LOW);
+  digitalWrite(yellowLED,LOW);
+  digitalWrite(greenLED,HIGH);
+  delay(3000);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(yellowLED,HIGH);
+  delay(1000);
+  digitalWrite(yellowLED,LOW);
+
+}
