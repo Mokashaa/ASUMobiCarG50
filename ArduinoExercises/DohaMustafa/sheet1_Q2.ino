4cde7d46e59fbfb32a24278443488a64fda50afe
DohaMustafa <36457313+DohaMustafa@users.noreply.github.com>
2018-02-20 22:19:44 +0200

    Add files via upload

---
+++ ArduinoExercises/DohaMustafa/sheet1_Q2.ino
@@ -0,0 +1,50 @@
+
+#define LED01 13 
+#define LED02 12
+#define LED03 11
+#define LED04 10
+
+void setup()
+{
+pinMode(LED01, OUTPUT);
+pinMode(LED02, OUTPUT); 
+pinMode(LED03, OUTPUT); 
+pinMode(LED04, OUTPUT); 
+}
+
+void loop()
+{
+digitalWrite(LED01, HIGH); 
+delay(30);
+digitalWrite(LED02, HIGH); 
+delay(30);
+digitalWrite(LED01, LOW); 
+delay(30);
+digitalWrite(LED03, HIGH); 
+delay(30);
+digitalWrite(LED02, LOW);
+delay(30);
+digitalWrite(LED04, HIGH); 
+delay(30);
+digitalWrite(LED03, LOW);
+delay(30);
+digitalWrite(LED04, LOW);
+delay(30);
+digitalWrite(LED04, HIGH);
+delay(30);
+digitalWrite(LED03, HIGH);
+delay(30);
+digitalWrite(LED04, LOW);
+delay(30);
+digitalWrite(LED02, HIGH);
+delay(30);
+digitalWrite(LED03, LOW);
+delay(30);
+digitalWrite(LED01, HIGH);
+delay(30);
+digitalWrite(LED02, LOW);
+delay(30);
+digitalWrite(LED01, HIGH);
+}
+
+
