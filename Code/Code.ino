21121f8d588c43a869529e193199ec7b0877223f
Salma Tarek <1500667@eng.asu.edu.eg>
2018-02-20 15:11:38 +0200

    Added function (LEFT)  #3

--- Code/Code.ino
+++ Code/Code.ino
@@ -4,22 +4,31 @@ int B1 = ; //B1 is the 2nd right motors pin
 int A2 = ; //A2 is the 1st left motors pin
 int B2 = ; //B2 is the 2nd left motors pin
 
+//Assuming that when A1,A2 are HIGH and B1,B2 are LOW the wheels move forward
 
 
 
 void setup() {
-  // put your setup code here, to run once:
-  // besmallah
-   pinMode(ENA, OUTPUT); //Enable pins
+  pinMode(ENA, OUTPUT); //Enable pins
   pinMode(ENB, OUTPUT); //Enable pins
   pinMode(A1, OUTPUT);
   pinMode(B1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(B2, OUTPUT);
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+ }
 
+void Left () {
+  //function to turn the car to the left (sharp angle)
+  analogWrite(ENA, 255); //adjusting the speed to the highest value
+  analogWrite(ENB, 255); //adjusting the speed to the highest value
+  //turning the right motors on
+  digitalWrite(A1, HIGH); 
+  digitalWrite(B1, LOW);
+  //turning the left motors off
+  digitalWrite(A2, LOW);
+  digitalWrite(B2, LOW);  
 }
