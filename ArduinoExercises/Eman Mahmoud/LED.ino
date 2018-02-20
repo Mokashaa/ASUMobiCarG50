ced0b37b6e6d4273f2f3646a30120d2ec3596627
EmanmMostafa <36457262+EmanmMostafa@users.noreply.github.com>
2018-02-20 21:17:06 +0200

    Add files via upload

---
+++ ArduinoExercises/Eman Mahmoud/LED.ino
@@ -0,0 +1,15 @@
+
+int LED=13;
+  int onTime=500;
+  int offTime=500;
+void setup() {
+  pinMode(LED,OUTPUT);
+  
+ }
+
+void loop() {
+  digitalWrite (LED,HIGH);
+  delay(onTime);
+  digitalWrite(LED,LOW);
+  delay(offTime);
+}
