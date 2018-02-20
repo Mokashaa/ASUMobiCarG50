090e003473393855c706236b9a23720d765b38ab
DohaMustafa <36457313+DohaMustafa@users.noreply.github.com>
2018-02-20 22:04:33 +0200

    Add files via upload

---
+++ ArduinoExercises/DohaMustafa/Traffic_Lights.ino
@@ -0,0 +1,28 @@
+int redledPin =4;
+int yellowledPin=5;
+int greenledPin=6;
+int x=0;
+
+void setup() {
+  pinMode(redledPin,OUTPUT);
+  pinMode(yellowledPin,OUTPUT);
+  pinMode(greenledPin,OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(greenledPin,HIGH); //the green light is on for  8secs and then goes off
+  delay(8000);
+  digitalWrite(greenledPin,LOW);
+  
+  for(x=0;x<10;x++) //yellow light blinks 10 times and then goes off
+  {
+    digitalWrite(yellowledPin,HIGH);
+    delay(2000);
+    digitalWrite(yellowledPin,LOW);
+  }
+  
+  digitalWrite(redledPin,HIGH); //red light blinks for 8 secs and then goes off
+  delay(8000);
+  digitalWrite(redledPin,LOW);
+}
