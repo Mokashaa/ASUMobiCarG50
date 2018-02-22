e0dc0a0a3c2f5099214a1a7658640b45c7269cad
EmanmMostafa <36457262+EmanmMostafa@users.noreply.github.com>
2018-02-23 00:39:32 +0200

    Add files via upload

---
+++ ArduinoExercises/Eman Mahmoud/sheet1-1.ino
@@ -0,0 +1,15 @@
+int led=8;
+int ledOn=500;
+int ledOff=1500;
+
+void setup() {
+  pinMode(led,OUTPUT);
+  
+ }
+
+void loop() {
+  digitalWrite (led,HIGH);
+  delay(ledOn);
+  digitalWrite(led,LOW);
+  delay(ledOff);
+}
